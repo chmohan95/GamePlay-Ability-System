61c4545ec5a1a10026ad32acc298b3719e316dc8
chmohan95 <62322576+chmohan95@users.noreply.github.com>
2024-01-25 17:20:01 +0530

    Aura Character & Enemy

---
+++ Source/Aura/Private/character/Aura1Character.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "character/Aura1Character.h"
+
+
---
+++ Source/Aura/Public/character/Aura1Character.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "Aura1Character.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAura1Character : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+
+public:
+
+	
+	
+
+private:
+	
+};
